5d0c73b5f07c7bed1261ca4839ed5af02efb27be
Qui <nguyenvanqui192@gmail.com>
2019-10-10 15:07:23 +0700

    app crashes when call video

--- vector/src/main/java/vmodev/clearkeep/di/AbstractRoomActivityModule.kt
+++ vector/src/main/java/vmodev/clearkeep/di/AbstractRoomActivityModule.kt
@@ -2,10 +2,13 @@ package vmodev.clearkeep.di
 
 import dagger.Module
 import dagger.android.ContributesAndroidInjector
+import im.vector.activity.JitsiCallActivity
 import vmodev.clearkeep.activities.RoomActivity
 
 @Module
 abstract class AbstractRoomActivityModule {
     @ContributesAndroidInjector
     abstract fun contributeRoomActivity(): RoomActivity;
+    @ContributesAndroidInjector
+    abstract fun contributeJitsiCallActivity(): JitsiCallActivity;
 }
