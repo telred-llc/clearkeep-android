1891656fb0a51dde7a6e283d9627d7667d0bf1a7
Qui <nguyenvanqui192@gmail.com>
2019-10-15 08:34:52 +0700

    build core ui

--- vector/src/main/java/im/vector/extensions/CharacterFormatExtension.kt
+++ vector/src/main/java/im/vector/extensions/CharacterFormatExtension.kt
@@ -3,5 +3,4 @@ package im.vector.extensions
 import android.text.TextUtils
 
 
-
-fun String.formatMessageEdit(message: String) =if(!TextUtils.isEmpty(message)) message.replace("*", "").trim() else message
+fun String.formatMessageEdit(message: String) = if (!TextUtils.isEmpty(message)) message.replaceFirst("*","")else message
