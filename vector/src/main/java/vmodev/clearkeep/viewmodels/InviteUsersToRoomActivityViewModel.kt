17a14e3bba2bcb4c1f174101c68adb6d35f73c11
longpq <longquangpham@gmail.com>
2019-11-29 11:48:32 +0700

    commit update logic create new call

--- vector/src/main/java/vmodev/clearkeep/viewmodels/InviteUsersToRoomActivityViewModel.kt
+++ vector/src/main/java/vmodev/clearkeep/viewmodels/InviteUsersToRoomActivityViewModel.kt
@@ -15,43 +15,46 @@ import javax.inject.Inject
 
 class InviteUsersToRoomActivityViewModel @Inject constructor(userRepository: UserRepository, roomRepository: RoomRepository, private val roomUserJoinRepository: RoomUserJoinRepository) : AbstractInviteUsersToRoomActivityViewModel() {
 
-    private val _query = MutableLiveData<String>();
-    private val _inviteUserToRoom = MutableLiveData<RoomRepository.InviteUsersToRoomObject>();
-    private val _joinRoom = MutableLiveData<String>();
+    private val _query = MutableLiveData<String>()
+    private val _inviteUserToRoom = MutableLiveData<RoomRepository.InviteUsersToRoomObject>()
+    private val _joinRoom = MutableLiveData<String>()
     private val users: LiveData<Resource<List<User>>> = Transformations.switchMap(_query) { input ->
-        userRepository.findUserFromNetwork(input);
+        userRepository.findUserFromNetwork(input)
     }
+
     private val inviteUsersToRoom: LiveData<Resource<Room>> = Transformations.switchMap(_inviteUserToRoom) { input ->
-        roomRepository.inviteUsersToRoom(input);
+        roomRepository.inviteUsersToRoom(input)
     }
+
     private val _joinRoomResult = Transformations.switchMap(_joinRoom) { input -> roomRepository.joinRoom(input) }
 
     override fun getUsers(): LiveData<Resource<List<User>>> {
-        return users;
+        return users
     }
 
     override fun getInviteUsersToRoomResult(): LiveData<Resource<Room>> {
-        return inviteUsersToRoom;
+        return inviteUsersToRoom
     }
 
     override fun setInviteUsersToRoom(roomId: String, userIds: List<String>) {
-        _inviteUserToRoom.value = RoomRepository.InviteUsersToRoomObject(roomId, userIds);
+        _inviteUserToRoom.value = RoomRepository.InviteUsersToRoomObject(roomId, userIds)
     }
 
     override fun setQuery(query: String) {
         if (!TextUtils.equals(_query.value, query)) {
-            _query.value = query;
+            _query.value = query
         }
     }
 
     override fun setJoinRoom(roomId: String) {
         if (!TextUtils.equals(_joinRoom.value, roomId))
-            _joinRoom.value = roomId;
+            _joinRoom.value = roomId
     }
 
     override fun joinRoomResult(): LiveData<Resource<Room>> {
-        return _joinRoomResult;
+        return _joinRoomResult
     }
+
     override fun getListUserSuggested(type: Int, userID: String): LiveData<Resource<List<User>>> {
         return roomUserJoinRepository.getListUserSuggested(type, userID)
     }
--- vector/src/main/java/vmodev/clearkeep/viewmodels/interfaces/AbstractCreateNewCallActivityViewModel.kt
+++ vector/src/main/java/vmodev/clearkeep/viewmodels/interfaces/AbstractCreateNewCallActivityViewModel.kt
@@ -1,16 +1,11 @@
 package vmodev.clearkeep.viewmodels.interfaces
 
 import androidx.lifecycle.LiveData
-import androidx.lifecycle.ViewModel
 import vmodev.clearkeep.viewmodelobjects.Resource
 import vmodev.clearkeep.viewmodelobjects.Room
-import vmodev.clearkeep.viewmodelobjects.User
 
-abstract class AbstractCreateNewCallActivityViewModel : ViewModel() {
-    abstract fun setQuery(query: String);
-    abstract fun getUsersByQueryResult(): LiveData<Resource<List<User>>>;
-    abstract fun setCreateNewRoom(name: String, topic: String, visibility: String);
-    abstract fun getCreateNewRoomResult(): LiveData<Resource<Room>>;
-    abstract fun getListUserSuggested(type: Int, userId: String):LiveData<Resource<List<User>>>
-    data class CreateNewRoom(val name: String, val topic: String, val visibility: String);
+abstract class AbstractCreateNewCallActivityViewModel : AbstractInviteUsersToRoomActivityViewModel() {
+    abstract fun setCreateNewRoom(name: String, topic: String, visibility: String)
+    abstract fun getCreateNewRoomResult(): LiveData<Resource<Room>>
+    data class CreateNewRoom(val name: String, val topic: String, val visibility: String)
 }
